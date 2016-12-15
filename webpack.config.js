f6786c43aa0fa95c79cb30950c1811f9d6af8d13
Todd Wallar <todd.wallar@gmail.com>
2016-12-15 12:52:42 -0800

    Added site and minify options to package.json config. Maybe should eventually be command line, but this seems good, too.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,40 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+
+var options = {
+  "site": process.env.npm_package_config_site,
+  "minify": (process.env.npm_package_config_minify === true)
+};
+
+console.log('minify ' + process.env.npm_package_config_minify);
+
+var plugins = [];
+
+if (options.minify) {
+  console.log('uglify');
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        mangle: {
+          //props: true
+          //toplevel: true
+        }
+      })
+    );
+}
+
+var filename =  options.site + '_bundle' + (options.minify?'_min':'') + '.js';
 
 module.exports = {
     context: __dirname,
-    entry: './sites/today.js',
+    entry: "./sites/" + options.site + ".js",
     output: {
-        path: __dirname + '/dist',
-        filename: 'today_bundle.js'
+        "path": __dirname + '/dist',
+        "filename":  filename
     },
     resolve: {
         modules: [
             path.resolve(__dirname)
         ]
     },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            mangle: {
-                //props: true
-                //toplevel: true
-            }
-        })
-    ]
+  "plugins": plugins
 }
