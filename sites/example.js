24988670ab9e2c72118aa22c4e3dff3cf921de6c
Todd Wallar <todd.wallar@gmail.com>
2017-01-06 16:02:47 -0800

    Moved dependency lists to modules.

--- sites/example.js
+++ sites/example.js
@@ -1,9 +1,3 @@
-import * as basic from 'tm_tools/basicUtils';
 import stuff from "tm_modules/example/stuff/module.js";
 
-var dependModules = [
-  'jQuery',
-  'something.not.defined'
-];
-
-basic.runWhenAvailable( dependModules, stuff);
+stuff();
--- sites/today.js
+++ sites/today.js
@@ -1,11 +1,3 @@
-import * as basic from 'tm_tools/basicUtils';
 import controller from "tm_modules/today/ads/ad-controller";
 
-var dependModules = [
-    'mps',
-    'jQuery',
-    'Backbone',
-    'jQuery.waypoints' //jquery version of waypoints
-];
-
-basic.runWhenAvailable( dependModules, controller);
+controller();
